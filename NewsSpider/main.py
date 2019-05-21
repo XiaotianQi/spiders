378853fcace94217275750f2ed5574817ef96299
XiaotianQi <bnwseven@gmail.com>
2019-05-21 09:40:10 +0800

    增添 test 文件忽略规则

--- NewsSpider/main.py
+++ NewsSpider/main.py
@@ -5,7 +5,8 @@ import sys
 from scrapy.cmdline import execute
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(['scrapy', 'crawl', 'test'])
 # execute(['scrapy', 'crawl', 'chinanews'])
 # execute(['scrapy', 'crawl', 'zhihu'])
 # execute(['scrapy', 'crawl', 'sina'])
-execute(['scrapy', 'crawl', 'sina_news_roll'])
+# execute(['scrapy', 'crawl', 'sina_news_roll'])
